d47a205aa654071a1a5ab3f38bbc8ea69376c2dd
snipersky <42208688+snipersky@users.noreply.github.com>
2018-08-15 01:52:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,5 +28,52 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
 });
 
+const Discord = require("discord.js");
+  const SNIPERSKY = new Discord.Client();
+    const SNIPERSKYtoken = "NDE3NjI3NTk1NTA1NTMyOTI4.DXXNWQ.kn7L_x1OV7fizN1G9BkFdPJrANE"
+      SNIPERSKY.on('ready', () => {
+        SNIPERSKY.user.setGame(`^bc | By snipersky`,'https://www.twitch.tv/TEST-Broadcast');
+          console.log('Im Ready!');
+  
+        });
+
+  SNIPERSKY.on('message', message => {
+    if (message.content.split(' ')[0] == '^bc')
+       message.guild.members.forEach( member => {
+         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+
+
+           member.send( `  ` + "   " + message.content.substr(3));
+                                                      message.delete();
+            
+                                                    });
+            
+                                                  });
+   SNIPERSKY.on("message", message => {
+       var prefix = "%";
+ 
+             var args = message.content.substring(prefix.length).split(" ");
+                if (message.content.startsWith(prefix + "b")) {
+                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+
+                          if (!args[1]) {
+                            
+                                 let embed3 = new Discord.RichEmbed()
+                                     .setDescription(":white_check_mark: | تم ارسال رسالة لا يوجد فيها شيء")
+                                       .setColor("#FF00FF")
+                                          message.channel.sendEmbed(embed3);
+                            
+                                        } else {
+
+                            
+                                           let embed4 = new Discord.RichEmbed()
+                                                            .setDescription(':white_check_mark: | تم ارسال الرساله للجميع ..')
+                                                                .setColor("#99999")
+                               
+                                                                message.channel.sendEmbed(embed4);
+                                                      message.delete();
+                            }
+                          }
+});
 
 client.login(process.env.BOT_TOKEN);
