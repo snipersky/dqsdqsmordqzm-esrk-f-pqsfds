6c75ffeb5b236917f3de215e72f262f89cde38a6
snipersky <42208688+snipersky@users.noreply.github.com>
2018-08-15 02:21:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '^'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`^bc | By snipersky`")
+client.user.setGame(`^bc | By snipersky`,"http://twitch.tv/SNIPERSKY")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
