4c55ebc6fef9d84b22c075f44efeae345e9955c2
snipersky <42208688+snipersky@users.noreply.github.com>
2018-08-15 02:06:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,21 +28,19 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
 });
 
-
-
-  SNIPERSKY.on('message', message => {
+client.on('message', message => {
     if (message.content.split(' ')[0] == '^bc')
        message.guild.members.forEach( member => {
          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
 
 
-           member.send( `  ` + "   " + message.content.substr(3));
+           member.send("   " + message.content.substr(3));
                                                       message.delete();
-            
+
                                                     });
-            
+
                                                   });
-   SNIPERSKY.on("message", message => {
+   client.on("message", message => {
        var prefix = "%";
  
              var args = message.content.substring(prefix.length).split(" ");
@@ -50,23 +48,22 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
                           if (!message.member.hasPermission("ADMINISTRATOR"))  return;
 
                           if (!args[1]) {
-                            
+
                                  let embed3 = new Discord.RichEmbed()
                                      .setDescription(":white_check_mark: | تم ارسال رسالة لا يوجد فيها شيء")
                                        .setColor("#FF00FF")
                                           message.channel.sendEmbed(embed3);
-                            
+
                                         } else {
 
-                            
+
                                            let embed4 = new Discord.RichEmbed()
                                                             .setDescription(':white_check_mark: | تم ارسال الرساله للجميع ..')
                                                                 .setColor("#99999")
-                               
+
                                                                 message.channel.sendEmbed(embed4);
                                                       message.delete();
                             }
                           }
 });
-
 client.login(process.env.BOT_TOKEN);
